7e067eaf6c13905c2f069351a41748bba45c8054
AbubakrBardien <abubakrbardien@gmail.com>
2025-01-27 16:54:54 +0200

    Worked on the UI

--- main.py
+++ main.py
@@ -3,30 +3,23 @@ from tkinter import ttk
 
 class MainWindow:
     root = ctk.CTk()
-    root.geometry("1000x600")
+    root.geometry("900x600")
 
     font_style_body = ("Helvetica", 16)
 
     label = ctk.CTkLabel(root, text="Expense Tracker", font=("Times New Roman", 28))
     label.pack(padx=10, pady=10)
 
-    frame = ctk.CTkFrame(root, fg_color="transparent")
-    frame.pack(pady=10, fill="both")
+    gridFMaster = ctk.CTkFrame(root, fg_color="transparent")
+    gridFMaster.pack(pady=8, fill="both")
 
-    frame1 = ctk.CTkFrame(frame, height=400, width=160, fg_color="transparent")
-    frame1.pack_propagate(False)
-    frame2 = ctk.CTkFrame(frame, fg_color="transparent")
+    column_indexes = (0, 1)
 
-    frame1.pack(side="left")
-    frame2.pack(side="right", anchor="n")
+    gridFMaster.columnconfigure(column_indexes, uniform='_', weight=1)
+    gridFMaster.rowconfigure(0, uniform='_', weight=2)
+    gridFMaster.rowconfigure((1, 2, 3, 4), uniform='_', weight=1)
 
-    btn1 = ctk.CTkButton(frame1, text="Btn1", font=font_style_body)
-    btn1.pack(pady=10)
-
-    btn2 = ctk.CTkButton(frame1, text="Btn2", font=font_style_body)
-    btn2.pack(pady=10)
-
-    table = ttk.Treeview(frame2, columns=("date", "description", "income", "expense", "balance"), show="headings")
+    table = ttk.Treeview(gridFMaster, columns=("date", "description", "income", "expense", "balance"), show="headings")
 
     table.heading("#1", text="Date")
     table.heading("#2", text="Description")
@@ -40,9 +33,67 @@ class MainWindow:
     table.column("#4", width=140)
     table.column("#5", width=140)
 
-    table.pack(padx=10, pady=10)
+    table.grid(row=0, column=0, sticky="news", columnspan=len(column_indexes))
 
     # table.insert("", index=ctk.END, values=("Abubakr", "Bardien", "abubakrbardien@gmail.com"))
+
+    gridFrameA = ctk.CTkFrame(gridFMaster)
+    gridFrameA.grid(row=1, column=0, sticky="news")
+
+    gridFrameA.columnconfigure((0, 1), uniform='_', weight=1)
+    gridFrameA.rowconfigure(0, uniform='_', weight=1)
+
+    # btn1 = ctk.CTkButton(gridFrame, text="Btn1", font=font_style_body)
+    # btn2 = ctk.CTkButton(gridFrame, text="Btn2", font=font_style_body)
+
+    lblDesc = ctk.CTkLabel(gridFrameA, text="Description:", font=font_style_body)
+    lblDesc.grid(row=0, column=0, sticky="e", padx=(0,5))
+
+    inputDesc = ctk.CTkEntry(gridFrameA, font=font_style_body)
+    inputDesc.grid(row=0, column=1, sticky="w")
+
+    gridFrameB = ctk.CTkFrame(gridFMaster)
+    gridFrameB.grid(row=1, column=1, sticky="news")
+
+    gridFrameB.columnconfigure(0, uniform='_', weight=1)
+    gridFrameB.columnconfigure(1, uniform='_', weight=2)
+    gridFrameB.rowconfigure((0, 1), uniform='_', weight=1)
+
+
+    # gridFrameB.columnconfigure((0, 1), uniform='_', weight=1)
+    # gridFrameB.rowconfigure(0, uniform='_', weight=1)
+
+    radioVar = ctk.IntVar()
+
+    rbIncome = ctk.CTkRadioButton(gridFrameB, text="Income", font=font_style_body, value=1, variable=radioVar)
+    rbExpense = ctk.CTkRadioButton(gridFrameB, text="Expense", font=font_style_body, value=2, variable=radioVar)
+
+    # rbIncome.grid(row=0, column=0, sticky="sw", padx=(25, 0), pady=(0, 5))
+    # rbExpense.grid(row=1, column=0, sticky="nw", padx=(25, 0), pady=(5, 0))
+
+    rbIncome.grid(row=0, column=0, sticky="s", pady=(0, 5))
+    rbExpense.grid(row=1, column=0, sticky="n", pady=(5, 0))
+
+    packAmountFrame = ctk.CTkFrame(gridFrameB)
+    packAmountFrame.grid(row=0, column=1, sticky="news", rowspan=2)
+
+    lblAmount = ctk.CTkLabel(packAmountFrame, text="Amount:", font=font_style_body)
+    lblAmount.pack(side=ctk.LEFT, padx=(0, 5))
+    # lblAmount.grid(row=0, column=1, sticky="w", rowspan=2)
+
+    inputAmount = ctk.CTkEntry(packAmountFrame, font=font_style_body)
+    inputAmount.configure(state="disabled")
+    inputAmount.pack(side=ctk.LEFT)
+
+    def get_rbtn_val(self):
+        print(self.radioVar)
+
+    # lbl1.grid(row=1, column=0, sticky="new", padx=(10, 30), pady=10)
+    # lbl1.grid(row=1, column=0, sticky="news", padx=10, pady=10)
+    # lbl1.grid(row=1, column=0, padx=10, pady=10)
+    # input1.grid(row=1, column=1, sticky="news", padx=10, pady=10)
+    # btn1.grid(row=0, column=0, sticky="new", padx=(10, 30), pady=10)
+    # btn2.grid(row=1, column=0, sticky="new", padx=(10, 30))
 
     root.mainloop()
 
